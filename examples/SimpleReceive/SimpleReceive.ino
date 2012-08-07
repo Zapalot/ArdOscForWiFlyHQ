5f887ce1c56b10d6ef333ad6ebb2e6aa582dd6fd
Felix Bonowski <email@server>
2012-08-07 23:04:04 +0200

    Fixed some examples and deleted the rest
    Will add the other examples when they work again.

--- examples/SimpleReceive/SimpleReceive.ino
+++ examples/SimpleReceive/SimpleReceive.ino
@@ -1,55 +1,53 @@
 #include <WiFlyHQ.h>
 #include <ArdOSCForWiFlyHQ.h>
-
-
+#define DEBUG 1
+#ifndef TRACE
+#define TRACE(x) do { if (DEBUG) Serial.print( x); } while (0)
+#endif
+#ifndef TRACELN
+#define TRACELN(x) do { if (DEBUG) Serial.println( x); } while (0)
+#endif
 
 
 #include <SoftwareSerial.h>
 #define softSerialRx 2
 #define softSerialTx 3
 
-
+int ledPin=13;
 SoftwareSerial softSerial(softSerialRx,softSerialTx);
-Stream* wiflySerial =&softSerial;
 
-/* Change these to match your WiFi network */
-const char mySSID[] = "SommerInAltTreptow";
-const char myPassword[] = "0mme-5qut-ayrr";
 WiFly wifly;
-
-uint16_t localPort =8000;
-
-uint16_t remotePort =8000;
-const char remoteHost[]="192.168.1.17";
-
-
-
-OSCServer server;
-
-
+OSCServer server(&wifly);
+int t=0;
 void setup()
 { 
-	setupWiFly();
-	debugWiFly();	//show some debug information
+  configureWiFly(
+  &softSerial, //the serial the wifly is attached to. assumes a softare serial. can be changed to hardware by editing the function below
+  9600,        //baudRate   
+     "Your WiFi Name",
+    "Your WiFi Password",
+    "WiFly",      // for identification in the network
+    "192.168.2.201",         // IP Adress of the Wifly. if 0 (without quotes), it will use dhcp to get an ip
+    8000,                    // WiFly receive port
+    "255.255.255.255",       // Where to send outgoing Osc messages. "255.255.255.255" will send to all hosts in the subnet
+    8001                     // outgoing port
+    );
 
+  printWiFlyInfo(); //print some debug information 
 
-  {
-   //start server
-   server.begin();
-   
    //set callback function
    server.addCallback("/ard/aaa",&func1);
    
    pinMode(ledPin, OUTPUT);  
-  }
 
 }
 
   
 void loop()
 {
-  if(server.aviableCheck()>0)
+  if(server.availableCheck()>0)
   {
+    
     //debugoutln("alive!"); //callback after process
   }
 
@@ -67,91 +65,201 @@ void func1(OSCMessage *_mes)
 }
 
 
-void setupWiFly(){
+void configureWiFly(
+SoftwareSerial* wiflySerial,
+const uint32_t newSerialSpeed,
+const char* SSID,
+const char* password,
+const char* deviceID,      ///< for identifacation in the network
+const char* localIP,       ///< a string with numbers, if 0, we will use dhcp to get an ip
+const uint16_t localPort,
+const char* remoteHost,
+const uint16_t remotePort
+){
+  TRACE((F("Free memory: ")));
+  TRACELN((wifly.getFreeMemory()));
+
+  boolean saveAndReboot=false;
   char buf[32];
-  softSerial.begin(9600);
 
-  if (!wifly.begin(wiflySerial, &Serial)) {
-    Serial.println("Failed to start wifly");
+  //try out some different serial speeds until we find one that is working...
+  const uint32_t serialSpeeds[]={
+    newSerialSpeed,9600, 19200, 38400, 57600, 115200      };
+
+  int speedIndex=0;
+  boolean baudrateFound=false;
+  TRACELN(F("Trying to connect to WiFly"));
+  for (speedIndex=0;speedIndex<5;speedIndex++){
+    TRACE(F("Connecting to Wifly using Baudrate:"));
+    TRACELN(serialSpeeds[speedIndex]);
+    wiflySerial->begin(serialSpeeds[speedIndex]);
+    if(wifly.begin(wiflySerial, &Serial)){
+      baudrateFound=true;
+      break;
+    }
   }
 
-  wifly.setDeviceID("Wifly-UDP");
-  Serial.print("DeviceID: ");
-  Serial.println(wifly.getDeviceID(buf, sizeof(buf)));
+  if(!baudrateFound){
+    TRACELN(F("Could not find working Baud rate to connect to WiFly"));
+    return;
+  }
+  else{
+    TRACE(F("Working Baud rate is "));
+    TRACELN(serialSpeeds[speedIndex]);
+  }
+
+  wifly.startCommand();  //made this public to avoid the annoing waiting times
+  if(wifly.getBaud()!=newSerialSpeed){
+    TRACE(F("Setting new baud rate to "));
+    TRACELN(newSerialSpeed);
+    //set the new wifly baud rate
+    wifly.setBaud(newSerialSpeed);
+    wiflySerial->begin(newSerialSpeed);
+    //saveAndReboot=true;
+  }
+
+  wifly.getDeviceID(buf, sizeof(buf));
+  if(strcmp(buf, deviceID) != 0){
+    TRACE(F("Changing device ID to "));
+    TRACELN(deviceID);
+    wifly.setDeviceID(deviceID);
+    saveAndReboot=true;
+  }
+
+  //setup dhcp or an ip..
+  if(localIP[0]==0){
+    if(wifly.getDHCPMode()!=WIFLY_DHCP_MODE_ON){
+      TRACELN(F("Enabling DHCP"));
+
+      wifly.enableDHCP();
+      saveAndReboot=true;
+    }
+  }
+  else{
+    if(wifly.getDHCPMode()!=WIFLY_DHCP_MODE_OFF){
+      TRACE(F("Disabling DHCP; setting IP to "));
+      TRACELN(localIP);
+      wifly.disableDHCP();
+      wifly.setIP(localIP);
+      saveAndReboot=true;
+    }
+    //set ip only if necessary...
+    wifly.getIP(buf,sizeof(buf));  
+    if(strcmp(buf, localIP) != 0){
+      TRACE(F("Setting IP to "));
+      TRACELN(localIP);
+      wifly.setIP(localIP);
+      saveAndReboot=true;
+    }
+  }
+
+  //receive packets at this port...
+  if(wifly.getPort()!=localPort){
+    TRACE(F("Setting listen Port to "));
+    TRACELN(localPort);
+    wifly.setPort( localPort);	// Send UPD packets to this server and port
+    saveAndReboot=true;
+  }
+
+  //2 millis seems to be the minimum for 9600 baud
+  if (wifly.getFlushTimeout() != 2) {
+    TRACE(F("Setting Flush timeout to 2ms "));
+    wifly.setFlushTimeout(2);
+    saveAndReboot=true;
+  }
   
-  if (wifly.getFlushTimeout() != 10) {
-    Serial.println("Restoring flush timeout to 10msecs");
-    wifly.setFlushTimeout(10);
-    wifly.save();
+  //set SSID only if necessary...
+  wifly.getSSID(buf,sizeof(buf));  
+  if(strcmp(buf, SSID) != 0){
+    TRACE(F("Setting SSID to "));
+    TRACELN((SSID));
+    wifly.setSSID(SSID);
+    
+    TRACE(F("Setting Wifi Passwd to "));
+    TRACELN((password));
+    wifly.setPassphrase(password);
+    saveAndReboot=true;
+  }
+  
+  wifly.save();
+  wifly.finishCommand();
+  if(saveAndReboot==true){
     wifly.reboot();
+    wiflySerial->begin(newSerialSpeed);
   }
 
+
+  
+  //set SSID only if necessary...
+  wifly.getSSID(buf,sizeof(buf));  
+  if(strcmp(buf, SSID) != 0){
+    wifly.setSSID(SSID);
+    wifly.setPassphrase(password);
+  }
+      
+      
   /* Join wifi network if not already associated */
   if (!wifly.isAssociated()) {
     /* Setup the WiFly to connect to a wifi network */
-    Serial.println("Joining network");
-    wifly.setSSID(mySSID);
-    wifly.setPassphrase(myPassword);
-    wifly.enableDHCP();
+    TRACELN(F("Joining network"));
+    wifly.setSSID(SSID);
+    wifly.setPassphrase(password);
 
     if (wifly.join()) {
-      Serial.println("Joined wifi network");
+      TRACELN(F("Joined wifi network"));
     } 
     else {
-      Serial.println("Failed to join wifi network");
+      TRACELN(F("Failed to join wifi network"));
     }
   } 
   else {
-    Serial.println("Already joined network");
+    TRACELN(F("Already joined network"));
   }
+
+  //enable auto join
+  wifly.setJoin(WIFLY_WLAN_JOIN_AUTO);
   /* Setup for UDP packets, sent automatically */
   wifly.setIpProtocol(WIFLY_PROTOCOL_UDP);
   wifly.setHost(remoteHost, remotePort);	// Send UPD packets to this server and port
-  
-  //receive packets at this port...
-  if(wifly.getPort()!=localPort){
-    Serial.println("Setting port to localPort and rebooting...");
-    wifly.setPort( localPort);	// Send UPD packets to this server and port
-    wifly.save();
-    wifly.reboot();
-  }
-      wifly.save();
-    Serial.println("WiFly ready");
+
+
+    TRACELN(F("WiFly setup finished"));
 }
-void debugWiFly(){
+void printWiFlyInfo(){
+  wifly.startCommand();
   char buf[32];
   /* Ping the gateway */
   wifly.getGateway(buf, sizeof(buf));
 
-  Serial.print("ping ");
-  Serial.print(buf);
-  Serial.print(" ... ");
+  TRACE("ping ");
+  TRACE(buf);
+  TRACE(" ... ");
   if (wifly.ping(buf)) {
-    Serial.println("ok");
+    TRACELN("ok");
   } 
   else {
-    Serial.println("failed");
+    TRACELN("failed");
   }
 
-  Serial.print("ping google.com ... ");
+  TRACE("ping google.com ... ");
   if (wifly.ping("google.com")) {
-    Serial.println("ok");
+    TRACELN("ok");
   } 
   else {
-    Serial.println("failed");
+    TRACELN("failed");
   }
 
-  Serial.print("MAC: ");
-  Serial.println(wifly.getMAC(buf, sizeof(buf)));
-  Serial.print("IP: ");
-  Serial.println(wifly.getIP(buf, sizeof(buf)));
-  Serial.print("Netmask: ");
-  Serial.println(wifly.getNetmask(buf, sizeof(buf)));
-  Serial.print("Gateway: ");
-  Serial.println(wifly.getGateway(buf, sizeof(buf)));
-
-
-  Serial.print("Current Port:");
-  Serial.println(wifly.getPort());
+  TRACE("MAC: ");
+  TRACELN(wifly.getMAC(buf, sizeof(buf)));
+  TRACE("IP: ");
+  TRACELN(wifly.getIP(buf, sizeof(buf)));
+  TRACE("Netmask: ");
+  TRACELN(wifly.getNetmask(buf, sizeof(buf)));
+  TRACE("Gateway: ");
+  TRACELN(wifly.getGateway(buf, sizeof(buf)));
+  TRACE("Listen Port:");
+  TRACELN(wifly.getPort());
+  wifly.finishCommand();
 
 }
+
--- examples/SimpleSend/SimpleSend.ino
+++ examples/SimpleSend/SimpleSend.ino
@@ -1,17 +1,29 @@
-#include <RedFly.h>
-#include <ArdOSC.h>
+#include <WiFlyHQ.h>
+#include <ArdOSCForWiFlyHQ.h>
+
+//debug messages: switch them off by replacing "DEBUG 1" by "DEBUG 0"
+
+#define DEBUG 1
+#ifndef TRACE
+#define TRACE(x) do { if (DEBUG) Serial.print( x); } while (0)
+#endif
+#ifndef TRACELN
+#define TRACELN(x) do { if (DEBUG) Serial.println( x); } while (0)
+#endif
 
 
-byte myIp[] = { 192, 168, 0, 30 };
-uint16_t serverPort = 10000;
-uint16_t destPort = 12000;
-byte destIp[] = { 192, 168, 0, 2 };
+#include <SoftwareSerial.h>
+#define softSerialRx 2
+#define softSerialTx 3
 
-OSCClient client;
+int ledPin=13;
+SoftwareSerial softSerial(softSerialRx,softSerialTx);
 
+WiFly wifly;
+OSCClient client(&wifly);
 //create new osc message
 OSCMessage global_mes;
-  
+//some variables to send...  
 int v3=10;
 float v4=10.0;
 char str1[]="simple send 1!";
@@ -19,70 +31,29 @@ char str1[]="simple send 1!";
 int v1=0;
 float v2=0.0;
 
-
-//serial format: 9600 Baud, 8N2
-void debugout(char *s)  { RedFly.disable(); Serial.print(s);   RedFly.enable(); }
-void debugoutln(char *s){ RedFly.disable(); Serial.println(s); RedFly.enable(); }
-
-
 void setup()
 { 
-  uint8_t ret;
+  configureWiFly(
+  &softSerial, //the serial the wifly is attached to. assumes a softare serial. can be changed to hardware by editing the function below
+  9600,        //baudRate   
+     "Your WiFi Name",
+    "Your WiFi Password",
+    "WiFly",      // for identification in the network
+    "192.168.2.201",         // IP Adress of the Wifly. if 0 (without quotes), it will use dhcp to get an ip
+    8000,                    // WiFly receive port
+    "255.255.255.255",       // Where to send outgoing Osc messages. "255.255.255.255" will send to all hosts in the subnet
+    8001                     // outgoing port
+    );
 
-  //init the WiFi module on the shield
-  // ret = RedFly.init(br, pwr) //br=9600|19200|38400|57600|115200|200000|230400, pwr=LOW_POWER|MED_POWER|HIGH_POWER
-  // ret = RedFly.init(pwr) //9600 baud, pwr=LOW_POWER|MED_POWER|HIGH_POWER
-  // ret = RedFly.init() //9600 baud, HIGH_POWER
-  ret = RedFly.init();
-  if(ret)
-  {
-    debugoutln("INIT ERR"); //there are problems with the communication between the Arduino and the RedFly
-    for(;;); //do nothing forevermore
-  }
-  else
-  {
-    //scan for wireless networks (must be run before join command)
-    RedFly.scan();
+printWiFlyInfo(); //print some debug information 
 
-    //join network
-    // ret = join("wlan-ssid", "wlan-passw", INFRASTRUCTURE or IBSS_JOINER or IBSS_CREATOR, chn, authmode) //join infrastructure or ad-hoc network, or create ad-hoc network
-    // ret = join("wlan-ssid", "wlan-passw", IBSS_CREATOR, chn) //create ad-hoc network with password, channel 1-14
-    // ret = join("wlan-ssid", IBSS_CREATOR, chn) //create ad-hoc network, channel 1-14
-    // ret = join("wlan-ssid", "wlan-passw", INFRASTRUCTURE or IBSS_JOINER) //join infrastructure or ad-hoc network with password
-    // ret = join("wlan-ssid", INFRASTRUCTURE or IBSS_JOINER) //join infrastructure or ad-hoc network
-    // ret = join("wlan-ssid", "wlan-passw") //join infrastructure network with password
-    // ret = join("wlan-ssid") //join infrastructure network
-    ret = RedFly.join("wlan-ssid", "wlan-passw", INFRASTRUCTURE);
-    if(ret)
-    {
-      debugoutln("JOIN ERR");
-      for(;;); //do nothing forevermore
-    }
-    else
-    {
-      //set ip config
-      // ret = RedFly.begin(); //DHCP
-      // ret = RedFly.begin(1 or 2); //1=DHCP or 2=Auto-IP
-      // ret = RedFly.begin(ip);
-      // ret = RedFly.begin(ip, dnsserver);
-      // ret = RedFly.begin(ip, dnsserver, gateway);
-      // ret = RedFly.begin(ip, dnsserver, gateway, netmask);
-      ret = RedFly.begin(myIp);
-      if(ret)
-      {
-        debugoutln("BEGIN ERR");
-        RedFly.disconnect();
-        for(;;); //do nothing forevermore
-      }
-    }
-  }
+
 }
 
   
 void loop()
 {
-  global_mes.setAddress(destIp,destPort);
-  
+  //note that the destination adress is set by setting the remote host of the wifly!  
   global_mes.beginMessage("/ard/send1");
   global_mes.addArgInt32(v1);
   global_mes.addArgFloat(v2);
@@ -108,8 +79,7 @@ void send2()
   //loacal_mes,str is release by out of scope
   OSCMessage loacal_mes;
   
-  loacal_mes.setAddress(destIp,destPort);
-  
+ 
   loacal_mes.beginMessage("/ard/send2");
   loacal_mes.addArgInt32(v3);
   loacal_mes.addArgFloat(v4);
@@ -122,3 +92,202 @@ void send2()
   v3++;
   v4 += 0.1;
 }
+
+void configureWiFly(
+SoftwareSerial* wiflySerial,
+const uint32_t newSerialSpeed,
+const char* SSID,
+const char* password,
+const char* deviceID,      ///< for identifacation in the network
+const char* localIP,       ///< a string with numbers, if 0, we will use dhcp to get an ip
+const uint16_t localPort,
+const char* remoteHost,
+const uint16_t remotePort
+){
+  TRACE((F("Free memory: ")));
+  TRACELN((wifly.getFreeMemory()));
+
+  boolean saveAndReboot=false;
+  char buf[32];
+
+  //try out some different serial speeds until we find one that is working...
+  const uint32_t serialSpeeds[]={
+    newSerialSpeed,9600, 19200, 38400, 57600, 115200      };
+
+  int speedIndex=0;
+  boolean baudrateFound=false;
+  TRACELN(F("Trying to connect to WiFly"));
+  for (speedIndex=0;speedIndex<5;speedIndex++){
+    TRACE(F("Connecting to Wifly using Baudrate:"));
+    TRACELN(serialSpeeds[speedIndex]);
+    wiflySerial->begin(serialSpeeds[speedIndex]);
+    if(wifly.begin(wiflySerial, &Serial)){
+      baudrateFound=true;
+      break;
+    }
+  }
+
+  if(!baudrateFound){
+    TRACELN(F("Could not find working Baud rate to connect to WiFly"));
+    return;
+  }
+  else{
+    TRACE(F("Working Baud rate is "));
+    TRACELN(serialSpeeds[speedIndex]);
+  }
+
+  wifly.startCommand();  //made this public to avoid the annoing waiting times
+  if(wifly.getBaud()!=newSerialSpeed){
+    TRACE(F("Setting new baud rate to "));
+    TRACELN(newSerialSpeed);
+    //set the new wifly baud rate
+    wifly.setBaud(newSerialSpeed);
+    wiflySerial->begin(newSerialSpeed);
+    //saveAndReboot=true;
+  }
+
+  wifly.getDeviceID(buf, sizeof(buf));
+  if(strcmp(buf, deviceID) != 0){
+    TRACE(F("Changing device ID to "));
+    TRACELN(deviceID);
+    wifly.setDeviceID(deviceID);
+    saveAndReboot=true;
+  }
+
+  //setup dhcp or an ip..
+  if(localIP[0]==0){
+    if(wifly.getDHCPMode()!=WIFLY_DHCP_MODE_ON){
+      TRACELN(F("Enabling DHCP"));
+
+      wifly.enableDHCP();
+      saveAndReboot=true;
+    }
+  }
+  else{
+    if(wifly.getDHCPMode()!=WIFLY_DHCP_MODE_OFF){
+      TRACE(F("Disabling DHCP; setting IP to "));
+      TRACELN(localIP);
+      wifly.disableDHCP();
+      wifly.setIP(localIP);
+      saveAndReboot=true;
+    }
+    //set ip only if necessary...
+    wifly.getIP(buf,sizeof(buf));  
+    if(strcmp(buf, localIP) != 0){
+      TRACE(F("Setting IP to "));
+      TRACELN(localIP);
+      wifly.setIP(localIP);
+      saveAndReboot=true;
+    }
+  }
+
+  //receive packets at this port...
+  if(wifly.getPort()!=localPort){
+    TRACE(F("Setting listen Port to "));
+    TRACELN(localPort);
+    wifly.setPort( localPort);	// Send UPD packets to this server and port
+    saveAndReboot=true;
+  }
+
+  //2 millis seems to be the minimum for 9600 baud
+  if (wifly.getFlushTimeout() != 2) {
+    TRACE(F("Setting Flush timeout to 2ms "));
+    wifly.setFlushTimeout(2);
+    saveAndReboot=true;
+  }
+  
+  //set SSID only if necessary...
+  wifly.getSSID(buf,sizeof(buf));  
+  if(strcmp(buf, SSID) != 0){
+    TRACE(F("Setting SSID to "));
+    TRACELN((SSID));
+    wifly.setSSID(SSID);
+    
+    TRACE(F("Setting Wifi Passwd to "));
+    TRACELN((password));
+    wifly.setPassphrase(password);
+    saveAndReboot=true;
+  }
+  
+  wifly.save();
+  wifly.finishCommand();
+  if(saveAndReboot==true){
+    wifly.reboot();
+    wiflySerial->begin(newSerialSpeed);
+  }
+
+
+  
+  //set SSID only if necessary...
+  wifly.getSSID(buf,sizeof(buf));  
+  if(strcmp(buf, SSID) != 0){
+    wifly.setSSID(SSID);
+    wifly.setPassphrase(password);
+  }
+      
+      
+  /* Join wifi network if not already associated */
+  if (!wifly.isAssociated()) {
+    /* Setup the WiFly to connect to a wifi network */
+    TRACELN(F("Joining network"));
+    wifly.setSSID(SSID);
+    wifly.setPassphrase(password);
+
+    if (wifly.join()) {
+      TRACELN(F("Joined wifi network"));
+    } 
+    else {
+      TRACELN(F("Failed to join wifi network"));
+    }
+  } 
+  else {
+    TRACELN(F("Already joined network"));
+  }
+
+  //enable auto join
+  wifly.setJoin(WIFLY_WLAN_JOIN_AUTO);
+  /* Setup for UDP packets, sent automatically */
+  wifly.setIpProtocol(WIFLY_PROTOCOL_UDP);
+  wifly.setHost(remoteHost, remotePort);	// Send UPD packets to this server and port
+
+
+    TRACELN(F("WiFly setup finished"));
+}
+void printWiFlyInfo(){
+  wifly.startCommand();
+  char buf[32];
+  /* Ping the gateway */
+  wifly.getGateway(buf, sizeof(buf));
+
+  TRACE("ping ");
+  TRACE(buf);
+  TRACE(" ... ");
+  if (wifly.ping(buf)) {
+    TRACELN("ok");
+  } 
+  else {
+    TRACELN("failed");
+  }
+
+  TRACE("ping google.com ... ");
+  if (wifly.ping("google.com")) {
+    TRACELN("ok");
+  } 
+  else {
+    TRACELN("failed");
+  }
+
+  TRACE("MAC: ");
+  TRACELN(wifly.getMAC(buf, sizeof(buf)));
+  TRACE("IP: ");
+  TRACELN(wifly.getIP(buf, sizeof(buf)));
+  TRACE("Netmask: ");
+  TRACELN(wifly.getNetmask(buf, sizeof(buf)));
+  TRACE("Gateway: ");
+  TRACELN(wifly.getGateway(buf, sizeof(buf)));
+  TRACE("Listen Port:");
+  TRACELN(wifly.getPort());
+  wifly.finishCommand();
+
+}
+
